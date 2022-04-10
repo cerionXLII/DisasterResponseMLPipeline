c908f027818ce153274c802bf9fec8c14eae5eb1
cerion <cerion42@gmail.com>
2022-04-10 21:28:34 +0200

    Converted jupyter notebooks into .py-files.

---
+++ src/etl_pipeline.py
@@ -0,0 +1,100 @@
+# import libraries
+import pandas as pd
+import numpy as np
+from sqlalchemy import create_engine
+
+# App settings
+messagesPath = 'Data/messages.csv' # Path to the messages data file
+categoriesPath = 'Data/categories.csv' # Path to the categories data file
+dataBasePath = 'sqlite:///Data/DisasterResponse.db'
+databaseTableName = 'messagesDataTable'
+
+
+def LoadData(messagesPath, categoriesPath):
+    # load messages dataset
+    messages = pd.read_csv(messagesPath)
+    
+    # load categories dataset
+    categories = pd.read_csv(categoriesPath)
+    
+    # merge datasets
+    df = messages.merge(categories, left_on='id', right_on='id', how='inner')
+    
+    return df
+    
+def PreprocessData(df):
+    # create a dataframe of the 36 individual category columns
+    categories = df['categories'].str.split(';', expand=True)
+    
+    # select the first row of the categories dataframe
+    row = categories.iloc[0]
+
+    # use this row to extract a list of new column names for categories.
+    # one way is to apply a lambda function that takes everything 
+    # up to the second to last character of each string with slicing
+    category_colnames = row.apply(lambda x:str(x).split('-')[0])
+    
+    # rename the columns of `categories`
+    categories.columns = category_colnames
+    
+    #Convert category values to just numbers 0 or 1
+    for column in categories:
+        # set each value to be the last character of the string
+        try:
+            categories[column] = categories[column].apply(lambda x:str(x).split('-')[-1])
+
+            # convert column from string to numeric
+            categories[column] = categories[column].astype('int32')
+
+        except exception as e:
+            print(e)
+        
+    
+    # drop the original categories column from `df`
+    df = df.drop(columns=['categories'])
+    
+    # concatenate the original dataframe with the new `categories` dataframe
+    df = df.merge(categories, left_index=True, right_index=True, how = 'left')
+    
+    # drop duplicates
+    df = df.drop_duplicates()
+    
+    return df
+    
+def SaveToDatabase(df, dataBasePath, databaseTableName):
+    engine = create_engine(dataBasePath)
+    df.to_sql(databaseTableName, engine, index=False,if_exists='replace')
+    
+    
+def main(messagesPath, categoriesPath, dataBasePath, databaseTableName):
+    print(f'Loading data. Messages: {messagesPath} Categories: {categoriesPath}')
+    df = LoadData(messagesPath, categoriesPath)
+    
+    print('Preprocessing data...')
+    df = PreprocessData(df)
+    
+    print(f'Saving data to database. Database path: {dataBasePath} Table Name: {databaseTableName}')
+    SaveToDatabase(df, dataBasePath, databaseTableName)
+    
+if __name__ == "__main__":
+    
+    import argparse
+
+    parser = argparse.ArgumentParser(description='ETL pipeline to preprocess disaster response messages.')
+    parser.add_argument('--messagesPath', help='Path to the messages csv file', default='Data/messages.csv')
+    parser.add_argument('--categoriesPath', help='Path to the categories csv file.', default='Data/categories.csv')
+    parser.add_argument('--dataBasePath', help='Path to the database', default='sqlite:///Data/DisasterResponse.db')
+    parser.add_argument('--databaseTableName', help='Name of the database table', default='messagesDataTable')
+
+    args = parser.parse_args()
+    print('Running ETL Pipeline...')
+
+    main(args.messagesPath, args.categoriesPath, args.dataBasePath, args.databaseTableName)
+    
+    print('Program terminated...')  
+    
+    
+    
+    
+    
+    
---
+++ src/train.py
@@ -0,0 +1,150 @@
+# import libraries
+import pandas as pd
+import numpy as np
+from sqlalchemy import create_engine 
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+import nltk
+import pickle
+from joblib import dump, load
+nltk.download('punkt')
+nltk.download('wordnet')
+nltk.download('omw-1.4')
+
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import SGDClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.metrics import classification_report, recall_score, precision_score, f1_score, average_precision_score
+from sklearn.feature_extraction.text import HashingVectorizer
+from sklearn.model_selection  import GridSearchCV
+
+
+
+
+def LoadData(dataBasePath, databaseTableName):
+    # load data from database
+    engine = create_engine(dataBasePath)
+    df = pd.read_sql(databaseTableName, engine)
+    return df
+
+def PreprocessData(df):
+    X = df['message']
+    Y = df.drop(columns=['id', 'message','original', 'genre'])
+    
+    #Remove classes that are not binary
+    columns_to_remove = []
+    for name in Y.columns:
+        if(Y[name].min() != 0 or
+            Y[name].max() != 1):
+            print(f'This is not a binary feature, will remove it: {name}')
+            columns_to_remove.append(name)
+
+
+    if len(columns_to_remove) > 0:
+        print(f'dropping {len(columns_to_remove)} Columns')
+        Y.drop(columns=columns_to_remove, inplace=True)
+        
+    return X, Y
+
+def tokenize(text):
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+    clean_tokens = [lemmatizer.lemmatize(x).lower().strip() for x in tokens]
+    return clean_tokens
+
+def CreatePipeline():
+    pipeline = Pipeline([
+            ('features', FeatureUnion([
+                ('text_pipeline', Pipeline([
+                    ('vect', CountVectorizer(tokenizer=tokenize)),
+                    ('tfidf', TfidfTransformer())
+                ])),
+                ('hashing_vectorizer', HashingVectorizer())
+            ])),
+           
+            ('clf', MultiOutputClassifier(SGDClassifier(penalty='l2',
+                                                        loss='log',
+                                                        alpha=1e-5, random_state=42,
+                                                        max_iter=5, tol=None)
+            ))])
+    return pipeline
+
+def EvaluateModel(model, X_test, Y_test):
+    Y_pred = model.predict(X_test)
+    
+    total_hits = np.sum(np.sum(Y_pred == Y_test))
+    total_misses = np.sum(np.sum(Y_pred != Y_test))
+    total_accuracy = total_hits/(total_hits + total_misses)
+       
+    target_names = [name for name in Y_test.columns]
+
+    precisions = []
+    recalls = []
+    f1scores = []
+    for (name, col) in zip(target_names, range(len(target_names))):
+        y_test = Y_test[name].values
+        y_pred = Y_pred[:, col]
+        
+        if(np.max(y_test) <= 1):
+            #Only one category
+            precisions.append(precision_score(y_test, y_pred))
+            recalls.append(recall_score(y_test, y_pred))
+            f1scores.append(f1_score(y_test, y_pred))
+        print(f'Category: {name}')
+        print(classification_report(y_test, y_pred))
+        print('-'*42)
+     
+    
+    print(f'Total Accuracy: {total_accuracy}')
+    print(f'Average Precission: {np.average(precisions)}')
+    print(f'Average Recall: {np.average(recalls)}')
+    print(f'Average F1 Score: {np.average(f1scores)}')
+    
+def main(modelName, dataBasePath, databaseTableName):    
+    
+    #Load data
+    print(f'Loading data from db: {dataBasePath} table: {databaseTableName}')
+    df = LoadData(dataBasePath, databaseTableName)
+    
+    #Create Features/Targets
+    print('Preprocessing data')
+    X, Y = PreprocessData(df)
+    
+    #Split into Train/test sets
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
+    
+    #Create model pipeline
+    print('Creating pipeline')
+    pipeline = CreatePipeline()
+    
+    #Fit to data
+    print('Training model')
+    pipeline.fit(X_train, Y_train)
+    
+    print('Evaluating model...')
+    EvaluateModel(pipeline, X_test, Y_test)
+    
+    #Save model to file
+    print(f'Saving model as: {modelName}')
+    dump(pipeline, modelName) 
+    
+if __name__ == "__main__":
+    
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Trains a model to classify disaster responses.')
+    parser.add_argument('--modelName', help='Name of the model', default='model.pkl')
+    parser.add_argument('--dataBasePath', help='Path to the database', default='sqlite:///Data/DisasterResponse.db')
+    parser.add_argument('--databaseTableName', help='Name of the database table', default='messagesDataTable')
+
+    args = parser.parse_args()
+    print('Training Model...')
+
+    main(args.modelName, args.dataBasePath, args.databaseTableName)
+    
+    print('Program terminated...')
+
+    
